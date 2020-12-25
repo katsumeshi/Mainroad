8aa5f5f556f1fa957da716edcd5bd6ca61c7e417
Yuki Matsushita <katsumeshi@gmail.com>
2020-12-25 10:36:30 -0800

    update gulp and scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@ var gulp         = require("gulp"),
     sass         = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer")
 
-gulp.task("scss", function () {
+gulp.task("scss", async function () {
     gulp.src("src/scss/**/*.scss")
         .pipe(sass({
             outputStyle : "compressed"
         }))
-        .pipe(autoprefixer({
-            browsers : ["last 20 versions"]
-        }))
+        .pipe(autoprefixer())
         .pipe(gulp.dest("static/css"))
 })
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss"], function () {
-    gulp.watch("src/scss/**/*", ["scss"])
-})
+gulp.task("watch", gulp.series("scss", async function () {
+    gulp.watch("src/scss/**/*", gulp.series("scss"))
+}))
 
-gulp.task("default", ["watch"])
+gulp.task("default", gulp.series("watch"))
